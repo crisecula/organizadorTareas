43c34c233c14c1a3023fdf92344994ed53641dee
crisecula <crisecula@macbook-de-cristina-de-la-fuente-gomez.local>
2014-05-23 02:02:11 +0200

    nueva pagina info alumna
    pagina info de aluna añadida

---
+++ alumna.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    
+    <head>
+        <?php include('headBasico.html'); ?>
+    </head>
+    
+    <body>
+        <img class="burbujas" src="images/para.png" alt="fondo burbujas">
+        
+        <header>
+            <?php include('menu.html'); ?>
+        </header>
+        
+        <section class="content" id="alumna">
+            <h1>ALUMNA</h1>
+            <h2>Cristina de la Fuente Gómez</h2>
+        </section>
+    </body>
+</html>
